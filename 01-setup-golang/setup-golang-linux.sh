e07619a308b0fb6120ed87be3b4407285f73d651
Kevin Leake <kevinleake01@gmail.com>
2014-04-09 16:46:39 +0000

    Textpatgen-Golang

---
+++ 01-setup-golang/setup-golang-linux.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# This file will download various documents, tutorials and instructions
+# about the Go Programming Language, plus how to set up using GCCGO and
+# LLVM/LLGO.
+#
+# This program runs in Linux, and will require Subversion, Mercurial (Hg),
+# and Git.
+#
+# ----- GOLANG.ORG -----
+hg clone https://code.google.com/p/go/
+hg clone https://code.google.com/p/go.wiki/
+hg clone https://code.google.com/p/go.text/
+hg clone https://code.google.com/p/go.example/
+hg clone https://code.google.com/p/go.empty/
+hg clone https://code.google.com/p/go.crypto/
+hg clone https://code.google.com/p/go.net/
+hg clone https://code.google.com/p/go.codereview/
+hg clone https://code.google.com/p/go.image/
+hg clone https://code.google.com/p/go.talks/
+hg clone https://code.google.com/p/go.exp/
+hg clone https://code.google.com/p/go.tools/
+hg clone https://code.google.com/p/go.benchmarks/
+
+# ----- GCCGO -----
+wget http://golang.org/doc/install/gccgo -O gccgo-install.html
+
+# ----- LLVM/LLGO -----
+git clone https://github.com/go-llvm/llvm.git
+git clone https://github.com/go-llvm/llgo.git
+
---
+++ 02-workspace-go/tpl-n0001.go
@@ -0,0 +1,55 @@
+/*
+####################################
+#
+# --- TEXTPATGEN TEMPLATE ---
+#
+# Users can change the output by editing
+# this file directly.
+#
+####################################
+*/
+
+package main
+
+import (
+  "fmt"
+)
+
+var size int64;
+var message string = "X-"; /* Message text */
+var separator string = " ";  /* Separator text */
+
+func main() {
+  start:=int64(0);  /* Start number */
+  finish:=int64(255); /* Finish number */
+  width:=int64(16);  /* Numbers in a line */
+
+  fmt.Printf("####################################\n");
+  fmt.Printf("#\n");
+  fmt.Printf("# -- TEXTPATGEN GENERATED FILE --\n");
+  fmt.Printf("#\n");
+  fmt.Printf("# Start number: %d ( 0x%X Hex, 0%o Octal )\n",start,start,start);
+  fmt.Printf("# Finish number: %d ( 0x%X Hex, 0%o Octal )\n",finish,finish,finish);
+  fmt.Printf("# Numbers in a line: %d\n",width);
+  fmt.Printf("#\n");
+  fmt.Printf("####################################\n");
+  numgen(start, finish, size, width);
+  fmt.Printf("# -- End of file.\n");
+}
+
+func numgen(start int64, finish int64, size int64, width int64) {
+  for num:=start; num<=finish; num++ {
+    for size:=int64(0); size<(width - 1); size++ {
+      if (num == finish) {
+         break;
+      }
+      fmt.Printf("%s", message);
+      fmt.Printf("%04X", num);  /* Number base format */
+      fmt.Printf("%s", separator);
+      num++;
+    }
+    fmt.Printf("%s", message);
+    fmt.Printf("%04X\n", num);  /* Number base format */
+  }
+}
+
